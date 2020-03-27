d7c9c8202712c254a331928f129ab70924a10bdb
pratikbaid3 <pratikbaid3@gmail.com>
2020-03-27 13:36:49 +0530

    Searchbar Ui changes

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -89,13 +89,13 @@ class _HomePageState extends State<HomePage> {
                           const EdgeInsets.only(right: 16, top: 8, bottom: 8),
                       child: Container(
                         decoration: BoxDecoration(
-                          color: Colors.blueGrey,
+                          color: Colors.blue.withOpacity(0.8),
                           borderRadius: const BorderRadius.all(
                             Radius.circular(38.0),
                           ),
                           boxShadow: <BoxShadow>[
                             BoxShadow(
-                                color: Colors.grey.withOpacity(0.2),
+                                color: Colors.blue.withOpacity(0.6),
                                 offset: const Offset(0, 2),
                                 blurRadius: 8.0),
                           ],
@@ -130,13 +130,13 @@ class _HomePageState extends State<HomePage> {
                   ),
                   Container(
                     decoration: BoxDecoration(
-                      color: Colors.blueGrey,
+                      color: Colors.blue.withOpacity(.8),
                       borderRadius: const BorderRadius.all(
                         Radius.circular(38.0),
                       ),
                       boxShadow: <BoxShadow>[
                         BoxShadow(
-                            color: Colors.grey.withOpacity(0.4),
+                            color: Colors.blue.withOpacity(0.6),
                             offset: const Offset(0, 2),
                             blurRadius: 8.0),
                       ],
--- lib/Pages/main.dart
+++ lib/Pages/main.dart
@@ -17,7 +17,7 @@ class OnBoarding extends StatelessWidget {
           primaryColor: Color(0xff202020),
           scaffoldBackgroundColor: Color(0xff202020),
           accentColor: Color(0xFF3F8AE1)),
-      initialRoute: '/HomePage',
+      initialRoute: '/OnBoardingPage',
       routes: {
         '/OnBoardingPage': (context) => OnBoardingPage(),
         '/SignupPage': (context) => SignupPage(),
